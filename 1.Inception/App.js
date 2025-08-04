36a255679644b80106028c038233d080b61cfb6a
RoshanGorakhpuriya <roshangorakhpuriya@gmail.com>
2025-08-04 21:47:08 +0530

    Episode1

---
+++ 1.Inception/App.js
@@ -0,0 +1,33 @@
+{/* <div id="outer">
+    <div id="inner1">
+        <h1>I'm 1inside div</h1>
+        <h2>i'm 1inside div2</h2>
+    </div>
+    <div id="inner2">
+        <h1>I'm 2inside div</h1>
+        <h2>i'm 2inside div2</h2>
+    </div>
+</div> */}
+
+
+// Nested 
+const heading2 = React.createElement("div" , {id:"outer"} , [ 
+    React.createElement("div" , {id : "inner"} , 
+    [React.createElement("h1" , {}  , "I'm inside div"),
+    React.createElement("h1" , {}  , "I'm inside div2")] 
+    ),
+    React.createElement("div" , {id : "inner"} , 
+        [React.createElement("h1" , {}  , "I'm inside div"),
+        React.createElement("h1" , {}  , "I'm inside div2")] 
+        )]
+)
+
+const root = ReactDOM.createRoot(document.getElementById("header"));
+root.render(heading2);
+
+// const heading = React.createElement("h1" , {id:"heading"} , "Hello From React");
+// //console.log(heading);
+// const root =  ReactDOM.createRoot(document.getElementById("root"));
+// // It's used to create a root for rendering React components in the DOM.
+// console.log(root);
+// root.render(heading);
